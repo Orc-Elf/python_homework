879045f162a575fe6a8f11e663cfa39ba92f18d4
sun-ji920 <1370495224@qq.com>
2023-10-20 13:38:29 +0800

    loopy

--- loop.py
+++ loop.py
@@ -1,3 +1,21 @@
+#question1
+num_1=2
+den_1=1
+num_2=3
+den_2=2
+sum=num_1/den_1+num_2/den_2
+
+for i in range(1,19):
+    num=num_1+num_2
+    den=den_1+den_2
+    sum+=num/den
+
+    num_1,den_1=num_2,den_2
+    num_2,den_2=num,den
+    
+print(sum)
+
+#question2
 str=input("please enter 1232a string")
 length=len(str)
 list1=list(str)
@@ -12,3 +30,22 @@ while i<=length/2:
        print("no")
        break
 
+
+#question3
+from math import sqrt
+
+count=0
+n=2
+flag=True
+while count<30:
+   flag=True
+   for i in range(2,int(sqrt(n))+1):
+        if n%i==0:
+            flag=False
+            break
+   if flag:
+        print(n,end='\t')
+        count+=1
+        if count%10==0:
+            print(end='\n')
+   n+=1
