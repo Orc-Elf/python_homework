76ddf21bd2247d8cb4fb2f0926ae8b473b3ddf06
Li Jiaxing <li13835183722@outlook.com>
2023-10-27 13:31:08 +0800

    order

--- 喆/cfchange.py
+++ 喆/cfchange.py
@@ -1,26 +1,23 @@
 def CtoF(c):
     'turn celsius into fahrenhei'
-    f=float((9/5)*c+32)
+    f = float((9 / 5) * c + 32)
     return f
+
 
 def FtoC(f):
     'turn fahrenheit into celsius'
-    c=float((5/9)*(f-32))
+    c = float((5 / 9) * (f - 32))
     return c
 
-print("%-15s%-15s|   %-15s%-20s"%('Celsius','Fahrenheit','Fahreenheit','Celsius'))
 
-#c=float(input('please enter celsius'))
-#f=float(input('please enter fahrenheit'))
-clist=[c for c in range(40,30,-1)]
-flist=[f for f in range(120,20,-10)]
+print("%-15s%-15s|   %-15s%-20s" % ('Celsius', 'Fahrenheit', 'Fahreenheit', 'Celsius'))
 
-       
-format="%-15.1f%-15.1f|   %-15.1f%-15.2f"
-for i in range(0,10):
- values=(clist[i],CtoF(clist[i]),flist[i],FtoC(flist[i]))
- print(format%values)
+# c=float(input('please enter celsius'))
+# f=float(input('please enter fahrenheit'))
+clist = [c for c in range(40, 30, -1)]
+flist = [f for f in range(120, 20, -10)]
 
-
-
-
+format = "%-15.1f%-15.1f|   %-15.1f%-15.2f"
+for i in range(0, 10):
+    values = (clist[i], CtoF(clist[i]), flist[i], FtoC(flist[i]))
+    print(format % values)
--- 喆/twinPrime.py
+++ 喆/twinPrime.py
@@ -1,19 +1,19 @@
 from math import sqrt
 
+
 def isPrime(num):
-    flag=True
-    for i in range(2,int(sqrt(num))+1):
-        if num%i==0:
-            flag=False
+    flag = True
+    for i in range(2, int(sqrt(num)) + 1):
+        if num % i == 0:
+            flag = False
             break
     if flag:
         return True
     else:
         return False
 
-for i in range(1,1000):
-    if isPrime(i):
-        if isPrime(i+2):
-            print('(',i,',',(i+2),')')
 
-    
+for i in range(1, 1000):
+    if isPrime(i):
+        if isPrime(i + 2):
+            print('(', i, ',', (i + 2), ')')
